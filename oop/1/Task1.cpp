7d8f06007a98f74e0fb5b8ed004d1492ce715562
dmitryukvyacheslav <dmitryuk.slavik@yandex.ru>
2023-09-08 00:27:57 +0700

    oop 1

---
+++ oop/1/Task1.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RAND_MAX INT_MAX
+
+int* genRandArray(int size, int maxValue) {
+    
+    int* arrayptr = (int*) malloc((size+1) * sizeof(int));
+    if (!arrayptr) return NULL;
+    arrayptr[0] = size;
+    for (int i = 1; i <= size; i++) 
+        arrayptr[i] = rand() % (maxValue+1);
+    return arrayptr;
+}   
+
+void print(int* arr) {
+    printf("%d: ", arr[0]);
+    for (int i = 1; i <= arr[0]; i++)
+        printf("%d ", arr[i]);
+}
+        
+int main() {
+    srand(time(NULL));
+    while (1) {
+
+        int size = rand() % 10;
+        int maxValue = 100;
+        int* arr = genRandArray(size, maxValue);
+        print(arr);
+        // очистка выделенной памяти
+        free(arr);
+
+        int c = getchar();
+        if (c == 120) // X key
+            break;
+    }
+    return 0;
+}
---
+++ oop/1/Task2.cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define RAND_MAX INT_MAX
+
+int* genRandArray(int size, int maxValue) {
+    
+    int* arrayptr = (int*) malloc((size+1) * sizeof(int));
+    if (!arrayptr) return NULL;
+    arrayptr[0] = size;
+    for (int i = 1; i <= size; i++) 
+        arrayptr[i] = rand() % (maxValue+1);
+    return arrayptr;
+}   
+
+void print(int* arr) {
+    printf("%d: ", arr[0]);
+    for (int i = 1; i <= arr[0]; i++)
+        printf("%d ", arr[i]);
+}
+      
+int** genRandMatrix(int size, int maxValue) {
+    int** arrayptr = (int**)malloc(size * sizeof(int*));
+    if (!arrayptr) return NULL;
+    for (int i = 0; i < size; i++)
+        arrayptr[i] = genRandArray(rand() % 10, maxValue);
+    return arrayptr;
+}
+
+void printMatrix(int** matrix, int size) {
+    printf("%d\n", size);
+    for (int i = 0; i < size; i++) {
+        print(matrix[i]);
+        putchar('\n');
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    int size = rand() % 10;
+    int maxValue = 100;
+    int* arr = genRandArray(size, maxValue);
+    print(arr);
+    // очистка выделенной памяти
+    free(arr); 
+    getchar();
+    return 0;
+}
