c6ab47e740079a280d990e82703cfc03c7c47f72
Dmitryuk Vyacheslav <dmitryuk.slavik@yandex.ru>
2023-10-27 02:29:22 +0700

    Add get_child and set_child to dsa-03

--- dsa/02/src/trie.cpp
+++ dsa/02/src/trie.cpp
@@ -11,20 +11,39 @@ Trie::trie* Trie::create_node(){
 }
 
 Trie::trie* Trie::get_child(trie* node, char c) {
-    static trie* curNode = node;
-    while (curNode) 
-        if (curNode->ch == c) 
-            return curNode->child;
-        else
-            curNode = curNode->sibling;
-    return NULL;    /* No such child...*/
+    // node->child - связный список
+    // у одного из его элементов должна быть буква c
+    static Trie::trie* ll_item = node->child;
+    while (ll_item)
+        if (ll_item->ch == c) return ll_item;
+        else ll_item = ll_item->sibling; 
+    return NULL; // ничего не нашли   
 }
 
 Trie::trie* Trie::set_child(trie* node, char c, trie* child) {
-    static trie* curNode = node;
-    while (curNode->sibling) 
-        curNode = curNode->sibling;
-    curNode->sibling 
+    // node->child - связный список
+    // в него надо добавить ноду child c буквой c
+    static Trie::trie* ll_item = node->child;
+    
+    // что если детей нет и список надо создать?
+    if (!ll_item) {
+        node->child = child;
+        node->child->ch = c;
+    }
+
+    // может быть, такая буква уже есть?
+    // пройдемся по списку, и добавим элемент, если нету
+    else {
+        while(1) {
+            if (ll_item->ch == c) return ll_item; // есть
+            else 
+                if (ll_item->sibling) ll_item = ll_item->sibling;
+                else break; // дошли до конца, пора вставлять элемент
+        }
+        ll_item->sibling = child;
+        ll_item->sibling->ch = c;
+    }
+}
 
 Trie::trie* Trie::insert(const std::string& key, int value) {
     static trie* node = root;   /* start from the root*/
